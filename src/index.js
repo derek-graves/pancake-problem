3964626b11d6a804eb7795577bb91fb28ec1e387
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-05-02 08:56:39 -0400

    Create pancake Stack class

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 import "./style.css";
 
-console.log("Working");
+const Stack = (order) => {
+  const getOrder = () => order;
+  const addPancake = () => {
+    order.push(order.length + 1);
+  };
+  const removePancake = () => {
+    const largestPancake = Math.max(...order);
+    const temp = order.filter((pancake) => pancake !== largestPancake);
+    order = temp;
+  };
+  const flip = (flipBeneath) => {
+    if (flipBeneath >= 0 && flipBeneath < order.length) {
+      const flipped = order.slice(0, flipBeneath + 1);
+      flipped.reverse();
+      order.splice(0, flipBeneath + 1, ...flipped);
+    }
+  };
+
+  return { getOrder, addPancake, removePancake, flip };
+};
+
+const test = Stack([1, 2, 4, 3]);
+test.flip(3);
+console.log(test.getOrder());
