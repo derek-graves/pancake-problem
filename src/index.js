6288349fae21e05d93f32d583317052adba22a3c
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-06-09 19:56:21 -0400

    Add click handler for pancakes

--- src/index.js
+++ src/index.js
@@ -1,2 +1,25 @@
 import "./style.css";
 import { Stack, SandboxStack } from "./stack";
+import { renderStack, renderHistory } from "./render";
+
+// create default stack
+const defaultOrder = [3, 2, 4, 1];
+const stack = new Stack(defaultOrder);
+
+const clickHandlerStack = (pancake) => {
+  const selectedPancake = pancake.target.dataset.stackIndex;
+
+  // only run when pancake is clicked
+  if (!selectedPancake) return;
+
+  stack.flip(selectedPancake);
+
+  renderStack(stack);
+  renderHistory(stack);
+};
+const stackDiv = document.getElementById("stack");
+stackDiv.addEventListener("click", clickHandlerStack);
+
+// render default stack and history on page load
+renderStack(stack);
+renderHistory(stack);
