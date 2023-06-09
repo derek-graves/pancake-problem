63c4f4a89c74da676391d390cf83be3fb48b6fba
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-06-09 15:00:29 -0400

    Write function to render stack

--- src/render.js
+++ src/render.js
@@ -1,38 +1,46 @@
+const updateStack = (stack) => {
+  const stackDOM = document.getElementById("stack");
+  stackDOM.innerHTML = "";
+
+  const stackArray = stack.order;
+  stackArray.forEach((pancake) => {
+    const newPancake = document.createElement("div");
+    newPancake.classList.add("pancake");
+
+    const widthPercentage = (pancake / stackArray.length) * 100;
+    newPancake.style.width = `${widthPercentage}%`;
+
+    stackDOM.append(newPancake);
+  });
+};
+
 const updateHistory = (stack) => {
   const historyDOM = document.getElementById("history");
-
-  // clear .history div
   historyDOM.innerHTML = "";
 
-  // get history array from stack
   const historyArray = stack.history;
-
-  // for each snapshot in history array:
+  // Traditional for loop used because each card's history-index data
+  // attribute comes from the index of the snapsnot associated with
+  // the card.
   for (let i = 0; i < historyArray.length; i++) {
-    // create card; add class(es) to card
     const newHistoryCard = document.createElement("div");
     newHistoryCard.classList.add("history-card");
 
-    //  add data attribute to card
     newHistoryCard.dataset.historyIndex = i + 1;
 
     const currentHistoryStack = historyArray[i];
     currentHistoryStack.forEach((pancake) => {
-      // create pancake; add class(es) to pancake
       const newHistoryPancake = document.createElement("div");
       newHistoryPancake.classList.add("card-pancake");
 
-      // set pancake width
       const widthPercentage = (pancake / currentHistoryStack.length) * 100;
       newHistoryPancake.style.width = `${widthPercentage}%`;
 
-      // add pancake to card
       newHistoryCard.append(newHistoryPancake);
     });
 
-    // add card to .history div
     historyDOM.append(newHistoryCard);
   }
 };
 
-export default updateHistory;
+export { updateStack, updateHistory };
