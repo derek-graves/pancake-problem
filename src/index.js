6e9af33d2dfa67c3488806c2b95d16b68ee5dd33
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-06-17 18:38:32 -0400

    Implement semi-functional drag logic

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ import changeInteractMode from "./interact-controls";
 
 // create default stack
 const defaultOrder = [6, 5, 3, 2, 4, 1];
-const stack = new Stack(defaultOrder);
+//const stack = new Stack(defaultOrder);
+const stack = new SandboxStack(defaultOrder);
 
 // add stack-handlers to stack and history elements
 const stackDiv = document.getElementById("stack");
@@ -54,3 +55,86 @@ interactModeDrag.addEventListener("click", (event) => {
 // render default stack and history on page load
 renderStack(stack);
 renderHistory(stack);
+
+//////////////////////////////////////////////////////////
+// let draggedElementIndex = "";
+
+const dragStart = (event) => {
+  const target = event.target;
+  if (target && target.classList.contains("pancake")) {
+    event.dataTransfer.effectAllowed = "move";
+    event.dataTransfer.dropEffect = "move";
+    event.dataTransfer.setData("text/plain", target.dataset.stackIndex);
+    // draggedElementIndex = target.dataset.stackIndex;
+
+    target.style.opacity = 0.1;
+  }
+};
+
+const dragEnd = (event) => {
+  if (event.target && event.target.classList.contains("pancake")) {
+    event.target.style.opacity = "";
+
+    const pancakes = [...document.getElementById("stack").children];
+    pancakes.forEach((pancake) => (pancake.style.transform = ""));
+  }
+};
+
+const dragOver = (event) => {
+  event.preventDefault();
+  /*
+  const overIndex = document.elementFromPoint(event.clientX, event.clientY)
+    .dataset.stackIndex;
+  if (overIndex === draggedElementIndex) {
+    return;
+  }
+  if (overIndex) {
+    const pancakes = [...document.getElementById("stack").children];
+    const moveUp = pancakes.filter(
+      (pancake) => pancake.dataset.stackIndex <= overIndex
+    );
+    const moveDown = pancakes.filter(
+      (pancake) => pancake.dataset.stackIndex >= overIndex
+    );
+    const height = pancakes[0].offsetHeight;
+    moveUp.forEach(
+      (element) =>
+        (element.style.transform = `translateY(-${height / 2 + 4}px)`)
+    );
+    moveDown.forEach(
+      (element) => (element.style.transform = `translateY(${height / 2 + 4}px)`)
+    );
+  }
+  */
+};
+
+const drop = (event) => {
+  event.preventDefault();
+  // validate target
+  if (!event.target.dataset.stackIndex) {
+    return;
+  }
+  const dropTarget = event.target;
+  const draggedIndex = parseInt(event.dataTransfer.getData("text"));
+  const droppedIndex = parseInt(dropTarget.dataset.stackIndex);
+
+  // prevent self-drops
+  if (droppedIndex !== draggedIndex) {
+    const stackDiv = document.getElementById("stack");
+    const clone = dropTarget.cloneNode(true);
+    const dragTarget = stackDiv.childNodes[draggedIndex];
+    stackDiv.replaceChild(dragTarget, dropTarget);
+    dragTarget.after(clone);
+  }
+  stack.movePancake(draggedIndex, droppedIndex);
+  renderStack(stack);
+  renderHistory(stack);
+};
+
+const allPancakes = [...stackDiv.children];
+allPancakes.forEach((pancake) => pancake.setAttribute("draggable", true));
+
+stackDiv.addEventListener("dragstart", dragStart);
+stackDiv.addEventListener("dragend", dragEnd);
+stackDiv.addEventListener("dragover", dragOver);
+stackDiv.addEventListener("drop", drop);
