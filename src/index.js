385a9e235e035f0556c898a0e190ec202475cc45
djgraves94 <88097664+djgraves94@users.noreply.github.com>
2023-07-01 14:15:49 -0400

    Instantiate and use invoker

--- src/index.js
+++ src/index.js
@@ -5,14 +5,16 @@ import addViewListeners from "./view-controls";
 import addInteractModeListeners from "./interact-mode-controls";
 import addHistoryListener from "./history-events";
 import { addFlipListeners, addDragListeners } from "./stack-events";
+import PancakeStackInvoker from "./stack-invoker";
 
 const defaultOrder = [6, 5, 3, 2, 4, 1];
 const stack = new SandboxStack(defaultOrder);
+const PancakeStackManager = new PancakeStackInvoker(stack);
 
 // add stack listners to stack and history elements to history
-addFlipListeners(stack);
-addDragListeners(stack);
-addHistoryListener(stack);
+addFlipListeners(PancakeStackManager, stack);
+addDragListeners(PancakeStackManager, stack);
+addHistoryListener(PancakeStackManager, stack);
 
 // add controls listeners
 addViewListeners(stack);
@@ -21,9 +23,11 @@ addInteractModeListeners(stack);
 // render default stack and history on page load
 renderStackAndHistory(stack);
 
+/* 
 const fullStack = (function () {
   const init = {
     stack: new Stack(defaultOrder),
   };
   return Object.seal(init);
 })();
+*/
